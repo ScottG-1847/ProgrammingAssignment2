ab9315dee14a7b3b2c5e89bc1ea0873215da6725
ScottG-1847 <s_goetsch@yahoo.com>
2015-03-22 12:34:40 -0500

    Update cachematrix file

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,57 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## Function to create a makeCachMatrix object
+## that can store and retrieve a matrix and its inverse
+makeCacheMatrix <- function(theMatrix = matrix()) {
+	## Initialize the stored inverse
+	theInverse <- NULL
+	
+	## Set a new value for the stored matrix
+	## and clear the stored inverse
+		setMatrix <- function(mx) {
+		theMatrix <<- mx
+		theInverse <<- NULL
+	}
+	
+	## Return the stored matrix
+		getMatrix <- function() {
+		theMatrix
+	}
+	
+	## Set a new value for the stored inverse
+		setInverse <- function(newInverse) {
+		theInverse <<- newInverse
+	}
+	
+	## Return the stored inverse
+		getInverse <- function() {
+		theInverse
+	}
+	
+	## Return a list of methods avaiable in this function
+		list(setMatrix = setMatrix,
+		getMatrix = getMatrix,
+		setInverse = setInverse,
+		getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+## Function to return the inverse of the matrix stored in a makeCacheMatrix (mCM) object
+## or calculate, store, and return a new inverse if no value is stored
+cacheSolve <- function(mx, ...) {
+	## Retrieve the inverse stored in the mCM object
+	cachedInverse <- mx$getInverse()
+	
+	## If a value is stored, return it
+	if(!is.null(cachedInverse)) {
+		message("getting cached data")
+		return(cachedInverse)
+	}
+	
+	## If no value is stored:
+	##   1) Calucate the inverse of the stored matrix
+	##   2) Store the new value in the mCM object
+	##   3) Return the new value
+	newMatrix <- mx$getMatrix()
+	newInverse <- solve(newMatrix, ...)
+	mx$setInverse(newInverse)
+	newInverse
+} 
